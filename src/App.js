affc87e27adaaf4f1e09639298f57a7aeb07da69
bsm021031 <bsm021031@naver.com>
2021-08-03 20:54:54 +0900

    commit 7

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(aws_exports);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Amplify 씹련들아</h1>
-        <h4>ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ
-        </h4>
+        <h1>Amplify</h1>
+        <h4>엄준식</h4>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});

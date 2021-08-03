6e7406212e9d8309c33f5ee0a0842c4f08705434
bsm021031 <bsm021031@naver.com>
2021-08-03 20:34:00 +0900

    commit 6

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Amplify 씹련들아</h1>
+        <h4>ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ
+        </h4>
       </header>
     </div>
   );
